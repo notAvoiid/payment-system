1035257e915cfaccbd076e424168864c247d9853
Igor Nascimento <igor.nacimento51@gmail.com>
2024-07-04 14:33:51 -0300

    feat: add pix payment

---
+++ src/main/java/com/abreu/payment_system/controller/PixController.java
@@ -0,0 +1,37 @@
+package com.abreu.payment_system.controller;
+
+import com.abreu.payment_system.model.dto.pix.PixChargeRequestDTO;
+import com.abreu.payment_system.service.PixService;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/pix")
+public class PixController {
+
+    private final PixService pixService;
+
+    public PixController(PixService pixService) {
+        this.pixService = pixService;
+    }
+
+    @GetMapping
+    public ResponseEntity pixCreateEVP(){
+
+        JSONObject response = this.pixService.pixCreateEVP();
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response.toString());
+    }
+
+    @PostMapping
+    public ResponseEntity pixCreateCharge(@RequestBody PixChargeRequestDTO pixChargeRequest){
+        JSONObject response = this.pixService.pixCreateCharge(pixChargeRequest);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(response.toString());
+    }
+}
---
+++ src/main/java/com/abreu/payment_system/model/dto/pix/PixChargeRequestDTO.java
@@ -0,0 +1,4 @@
+package com.abreu.payment_system.model.dto.pix;
+
+public record PixChargeRequestDTO(String chave, String valor){
+}
